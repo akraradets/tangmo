c38a7a82be5c2afd8da2374c05476b18c1c5618e
akraradets <akraradets@gmail.com>
2020-01-05 01:02:01 +0700

    Farmer index show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
     def farmer_picture(farmer, size=40, option="rounded")
         if farmer.picture.attached?
-            image_tag farmer.picture.variant( resize: "#{size}x#{size}!"), class: "#{option}" 
+            image_tag farmer.picture.variant( resize: "#{size}x#{size}!"), class: "img-fluid mx-auto d-block #{option}" 
         else
-            image_tag image_url("/profile-picture-placeholder.png"), class: "#{option}", height: "#{size}", width: "#{size}"
+            image_tag image_url("/profile-picture-placeholder.png"), class: "img-fluid mx-auto d-block  #{option}", height: "#{size}", width: "#{size}"
         end
     end
 end
--- app/models/farmer.rb
+++ app/models/farmer.rb
@@ -13,6 +13,18 @@ class Farmer < ApplicationRecord
         end
     end
 
+    def display_address
+        addrNo = addressNo != "" ? addressNo : "-" 
+        addrMoo = addressMoo != "" ? addressMoo : "-" 
+        addrTambon = addressTambon != "" ? addressTambon : "-" 
+        addrAmphoe = addressAmphoe != "" ? addressAmphoe : "-" 
+        addrProvince = addressProvince != "" ? addressProvince : "-" 
+        addrZipcode = addressZipcode != "" ? addressZipcode : "-" 
+    
+        addr = "บ้านเลขที่#{addrNo} หมู่#{addrMoo}  ตำบล#{addrTambon} อำเภอ#{addrAmphoe} จังหวัด#{addrProvince} รหัสไปรษณีย์#{addrZipcode}" 
+        return addr
+      end
+
     def age
       now = Time.now.utc.to_date
       age = now.year - dateOfBirth.year - (dateOfBirth.to_date.change(:year => now.year) > now ? 1 : 0)
