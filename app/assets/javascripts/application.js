43e3033422eec7307897079c7a60494e95c7437c
akraradets <akraradets@gmail.com>
2020-01-05 16:01:31 +0700

    place holder for plot

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,5 @@
 //= require activestorage
 //= require turbolinks
 //= require bootstrap
+//= require bootstrap-sprockets
 //= require_tree .
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,21 @@ module ApplicationHelper
         if farmer.picture.attached?
             image_tag farmer.picture.variant( resize: "#{size}x#{size}!"), class: "img-fluid mx-auto d-block #{option}" 
         else
-            image_tag image_url("/profile-picture-placeholder.png"), class: "img-fluid mx-auto d-block  #{option}", height: "#{size}", width: "#{size}"
+            image_tag "farmer-picture-placeholder.png", class: "img-fluid mx-auto d-block #{option}", height: "#{size}", width: "#{size}"
         end
+    end
+
+    def plot_picture(plot, size=40, option="rounded")
+        if plot.pictures.attached?
+            image_tag plot.pictures[0].variant( resize: "#{size}x#{size}!"), class: "img-fluid mx-auto d-block #{option}" 
+        else
+            image_tag "plot-picture-placeholder.jpg", class: "img-fluid mx-auto d-block #{option}", height: "#{size}", width: "#{size}"
+        end
+    end
+
+    def plot_picture_carousel(plot, size=40, option="rounded")
+
+
+
     end
 end
