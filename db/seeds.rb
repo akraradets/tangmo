9c6809189cb6517e0a33696a02cf6a8971fa8832
akraradets <akraradets@gmail.com>
2020-01-08 16:54:31 +0700

    nearlyDone

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,118 +1,138 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-
 User.create(email: 'admin@tangmo.com', username: 'admin', password: '123456', password_confirmation: '123456')
 puts "User - username='admin' password='123456'"
 
-fertilizeObj = {"0" => {"สูตรปุ๋ย" => "", "อัตรา" => ""},
-                "1" => {"สูตรปุ๋ย" => "", "อัตรา" => ""},
-                "2" => {"สูตรปุ๋ย" => "", "อัตรา" => ""},
-                "3" => {"สูตรปุ๋ย" => "", "อัตรา" => ""}
-            }
-
-farmer = Farmer.new(
-    title: "นาย",
-    firstName: "ณรงค์",
-    lastName: "มะขามป้อม",
+farmer1 = Farmer.new( 
+    title: 'นาย',
+    firstName: 'ณรงค์',
+    lastName: 'มะขามป้อม',
     dateOfBirth: Date.strptime('20-05-1965', '%d-%m-%Y'),
-    group: "ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค",
-    addressNo: "32/9",
-    addressMoo: "3",
-    addressTambon: "ศรีวิชัย",
-    addressAmphoe: "พุนพิน",
-    addressProvince: "สุราษฎร์ธานี",
-    addressZipcode: "",
-    phone: "082-422-4429",
-    facebook: "ณรงค์ มะขามป้อม",
-    line: "082-422-4429",
-    email: ""
-)
-farmer.picture.attach(io: File.open(Rails.root + 'seedPicture/farmer_1.jpg'), filename: 'farmer_1.jpg')
-farmer.save
-
-plot = Plot.new(
-    farmer: farmer,
+    group: 'ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค',
+    addressNo: '32/9',
+    addressMoo: '3',
+    addressTambon: 'ศรีวิชัย',
+    addressAmphoe: 'พุนพิน',
+    addressProvince: 'สุราษฎร์ธานี',
+    addressZipcode: '',
+    phone: '082-422-4429',
+    facebook: 'ณรงค์ มะขามป้อม',
+    line: '082-422-4429',
+    email: 'ไม่มี'
+    )
+Dir[Rails.root + "seedPicture/farmer1*"].each do |pic|
+    farmer1.picture.attach(io: File.open(pic), filename: pic.split('/').last)
+end
+farmer1.save
+plot1_1 = Plot.new(
+    farmer_id: 1,
     areaRai: 10,
     treeCount: 7000,
-    breed: "คอมมานโด",
-    project: "ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค ผู้บริโภคปลอดภัย ปี 2562",
-    certificate: "",
-    certificateDate: "19 มีนาคม 2562 - 18 มีนาคม 2564",
-    harvestPeriod: "Apr-62",
-    harvestQuantity: "2000",
-    price: "18",
-    plotManagement: '{"อินทรีย์และเคมี":"true","ตัดหญ้าดูแลแปลง":"true"}',
-    fertilizeManagement: '{"0":{"สูตรปุ๋ย":"25-7-7","อัตรา":"5"},"1":{"สูตรปุ๋ย":"15-15-15","อัตรา":"10"},"2":{"สูตรปุ๋ย":"14-14-21","อัตรา":"15"},"3":{"สูตรปุ๋ย":"14-14-21","อัตรา":"20"}}', 
-    waterManagement: '{"มีระบบน้ำหยด":"true"}',
-    illnessManagement: '{"พบโรคเถาเหี่ยว":"","พบโรคราน้ำค้าง":""}',
-    harvestManagement: '{"ใช้แรงงานคน":"true"}',
-    sellingChannel: '{"เกษตรกรจำหน่ายเอง":"true","เกษตรกรตัดพ่อค้าคนกลางมารับ":"true"}',
-    logistic: '{"มีรถมารับซื้อ":"true","รถกระบะ":"true"}',
-    addressNo: "",
-    addressMoo: "",
-    addressTambon: "มะลวน",
-    addressAmphoe: "",
-    addressProvince: "",
-    addressZipcode: "",
-    lat: 9.190191,
-    long: 99.224061
-)
-Dir[Rails.root + "seedPicture/plot_1*"].each do |pic|
-    plot.pictures.attach(io: File.open(pic), filename: pic.split('/').last)
+    breed: 'คอมมานโด',
+    project: 'ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค ผู้บริโภคปลอดภัย ปี 2562',
+    certificate: '',
+    certificateDate: '19 มีนาคม 2562 - 18 มีนาคม 2564',
+    harvestPeriod: 'April 2562',
+    harvestQuantity: '2000',
+    price: '18',
+    plotManagement: '{"อินทรีย์และเคมี": "true", "ตัดหญ้าดูแลแปลง": "true"}',
+    fertilizeManagement: '{"0": {"สูตรปุ๋ย": "25-7-7", "อัตรา": "5"}, "1": {"สูตรปุ๋ย": "15-15-15", "อัตรา": "10"}, "2": {"สูตรปุ๋ย": "14-14-21", "อัตรา": "15"}, "3": {"สูตรปุ๋ย": "14-14-21", "อัตรา": "20"}}',
+    waterManagement: '{"มีระบบน้ำหยด": "true"}',
+    illnessManagement: '{"พบโรคเถาเหี่ยว": "ใช้สารเคมี - อัตรา -", "พบโรคราน้ำค้าง": "ใช้สารเคมี - อัตรา -"}',
+    harvestManagement: '{"ใช้แรงงานคน": "true"}',
+    sellingChannel: '{"เกษตรกรจำหน่ายเอง": "true", "เกษตรกรตัดพ่อค้าคนกลางมารับ": "true"}',
+    logistic: '{"มีรถมารับซื้อ": "true", "รถกระบะ": "true"}',
+    addressNo: '',
+    addressMoo: '',
+    addressTambon: 'มะลวน',
+    addressAmphoe: '',
+    addressProvince: '',
+    addressZipcode: '',
+    lat: '9.190191',
+    long: '99.224061'
+    )
+Dir[Rails.root + "seedPicture/plot1_1*"].each do |pic|
+    plot1_1.pictures.attach(io: File.open(pic), filename: pic.split('/').last)
 end
-plot.save
-
-plot2 = Plot.new(
-    farmer: farmer,
-    areaRai: 8,
-    treeCount: 7000,
-    breed: "คอมมานโด",
-    project: "ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค ผู้บริโภคปลอดภัย ปี 2562",
-    certificate: "",
-    certificateDate: "19 มีนาคม 2562 - 18 มีนาคม 2564",
-    harvestPeriod: "Apr-62",
-    harvestQuantity: "2000",
-    price: "18",
-    plotManagement: '{"อินทรีย์และเคมี":"true","ตัดหญ้าดูแลแปลง":"true"}',
-    fertilizeManagement: fertilizeObj.to_json, 
-    waterManagement: {}.to_json,
-    illnessManagement: {}.to_json,
-    harvestManagement: {}.to_json,
-    sellingChannel: {}.to_json,
-    logistic: {}.to_json,
-    addressNo: "",
-    addressMoo: "",
-    addressTambon: "มะลวน",
-    addressAmphoe: "",
-    addressProvince: "",
-    addressZipcode: "",
-    lat: 9.190194,
-    long: 99.224060
-)
-plot2.save
-
-
-farmer2 = Farmer.new(
-    title: "นาย",
-    firstName: "ณรงค์2",
-    lastName: "มะขามป้อม2",
-    dateOfBirth: Date.strptime('20-05-1965', '%d-%m-%Y'),
-    group: "ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค",
-    addressNo: "32/9",
-    addressMoo: "3",
-    addressTambon: "ศรีวิชัย",
-    addressAmphoe: "พุนพิน",
-    addressProvince: "สุราษฎร์ธานี",
-    addressZipcode: "",
-    phone: "082-422-4429",
-    facebook: "ณรงค์ มะขามป้อม",
-    line: "082-422-4429",
-    email: ""
-)
+plot1_1.save
+farmer2 = Farmer.new( 
+    title: 'นาย',
+    firstName: 'สุริยงค์',
+    lastName: 'โมอ่อน',
+    dateOfBirth: Date.strptime('02-08-1982', '%d-%m-%Y'),
+    group: 'ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค',
+    addressNo: '12',
+    addressMoo: '3',
+    addressTambon: 'ศรีวิชัย',
+    addressAmphoe: 'พุนพิน',
+    addressProvince: 'สุราษฎร์ธานี',
+    addressZipcode: '',
+    phone: '087-563-7573',
+    facebook: 'Jatinjegter',
+    line: 'ไม่มี',
+    email: 'ไม่มี'
+    )
+Dir[Rails.root + "seedPicture/farmer2*"].each do |pic|
+    farmer2.picture.attach(io: File.open(pic), filename: pic.split('/').last)
+end
 farmer2.save
+plot2_1 = Plot.new(
+    farmer_id: 2,
+    areaRai: 5,
+    treeCount: 5000,
+    breed: 'กินรี',
+    project: 'ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค ผู้บริโภคปลอดภัย ปี 2562',
+    certificate: '',
+    certificateDate: '',
+    harvestPeriod: '',
+    harvestQuantity: '0',
+    price: '16',
+    plotManagement: '{}',
+    fertilizeManagement: '{"0": {"สูตรปุ๋ย": "-", "อัตรา": "-"}, "1": {"สูตรปุ๋ย": "-", "อัตรา": "-"}, "2": {"สูตรปุ๋ย": "14-14-21", "อัตรา": "-"}, "3": {"สูตรปุ๋ย": "", "อัตรา": ""}}',
+    waterManagement: '{}',
+    illnessManagement: '{}',
+    harvestManagement: '{}',
+    sellingChannel: '{}',
+    logistic: '{}',
+    addressNo: '',
+    addressMoo: '7',
+    addressTambon: 'มะลวน',
+    addressAmphoe: '',
+    addressProvince: '',
+    addressZipcode: '',
+    lat: '9.159496',
+    long: '99.187747'
+    )
+Dir[Rails.root + "seedPicture/plot2_1*"].each do |pic|
+    plot2_1.pictures.attach(io: File.open(pic), filename: pic.split('/').last)
+end
+plot2_1.save
+plot2_2 = Plot.new(
+    farmer_id: 2,
+    areaRai: 10,
+    treeCount: 5000,
+    breed: 'กินรี',
+    project: 'ชมรมแตงโมปลอดสารพิษผู้ผลิตปลอดโรค ผู้บริโภคปลอดภัย ปี 2562',
+    certificate: '',
+    certificateDate: 'ไม่มี',
+    harvestPeriod: 'April 2562',
+    harvestQuantity: '10000',
+    price: '16',
+    plotManagement: '{"อินทรีย์และเคมี": "true"}',
+    fertilizeManagement: '{"0": {"สูตรปุ๋ย": "25-7-7", "อัตรา": "-"}, "1": {"สูตรปุ๋ย": "15-15-15", "อัตรา": "-"}, "2": {"สูตรปุ๋ย": "14-14-21", "อัตรา": "-"}, "3": {"สูตรปุ๋ย": "", "อัตรา": ""}}',
+    waterManagement: '{"มีระบบน้ำหยด": "true"}',
+    illnessManagement: '{"พบโรคเถาเหี่ยว": "ใช้สารเคมี ไซโปรโคนาโซล อัตรา 10 มิลลิลิตรต่อน้ำ 20 ลิตร", "พบโรคราน้ำค้าง": "ใช้สารเคมี ไซม็อกซานิล+แมนโคแซบ 8% +6% WP อัตรา 20 กรัมต่อน้ำ 20 ลิตร", "พบโรคเหี่ยวเขียว": "ไม่ใช้สารเคมี อัตรา"}',
+    harvestManagement: '{"ใช้แรงงานคน": "true"}',
+    sellingChannel: '{"เกษตรกรจำหน่ายเอง": "true", "เกษตรกรตัดพ่อค้าคนกลางมารับ": "true"}',
+    logistic: '{"มีรถมารับซื้อ": "true", "รถกระบะ": "true"}',
+    addressNo: '',
+    addressMoo: '7',
+    addressTambon: 'มะลวน',
+    addressAmphoe: '',
+    addressProvince: '',
+    addressZipcode: '',
+    lat: '9.188585',
+    long: '99.208613'
+    )
+Dir[Rails.root + "seedPicture/plot2_2*"].each do |pic|
+    plot2_2.pictures.attach(io: File.open(pic), filename: pic.split('/').last)
+end
+plot2_2.save
--- python/run.py
+++ python/run.py
@@ -1,9 +1,11 @@
 import pandas
 import glob
+import math
 
-def attachPicture(objName):
+def attachPicture(objName, attachName='picture'):
+    
     template = f"""Dir[Rails.root + "seedPicture/{objName}*"].each do |pic|
-    {objName}.pictures.attach(io: File.open(pic), filename: pic.split('/').last)
+    {objName}.{attachName}.attach(io: File.open(pic), filename: pic.split('/').last)
 end"""
     return template
 
@@ -32,19 +34,36 @@ def createFarmer(records):
     # 5	ที่อยู่อาศัย
     addresses = records[5][3].strip().split(' ')
     # ['32/9', 'หมู่', '3', 'ต.ศรีวิชัย', 'อ.พุนพิน', 'จ.สุราษฎร์ธานี']
-    # addressNo: "32/9",
-    # addressMoo: "3",
-    # addressTambon: "ศรีวิชัย",
-    # addressAmphoe: "พุนพิน",
-    # addressProvince: "สุราษฎร์ธานี",
-    # addressZipcode: "",
-    hasMoo = 0 if addresses[1] == 'หมู่' else 2
-    addressNo = addresses[0]
-    addressMoo = addresses[2] if hasMoo == 0 else ""
-    addressTambon = addresses[3-hasMoo].split('.')[1]
-    addressAmphoe = addresses[4-hasMoo].split('.')[1]
-    addressProvince = addresses[5-hasMoo].split('.')[1]
-    addressZipcode = addresses[6-hasMoo].split('.') if 6-hasMoo < len(addresses) else ""
+    addressNo = ""
+    addressMoo = ""
+    addressTambon = ""
+    addressAmphoe = ""
+    addressProvince = ""
+    addressZipcode = ""
+
+    isAddressNo = True
+    nextIsMoo = False
+    mightBeZipcode = False
+    for data in addresses:
+        if(nextIsMoo):
+            nextIsMoo = False
+            addressMoo = data
+        elif(data.find('.') == -1):
+            if(data == "หมู่"):
+                nextIsMoo = True
+            elif(mightBeZipcode):
+                addressZipcode = data
+            elif(isAddressNo):
+                addressNo = data
+        else:
+            if(data.split('.')[0] == "ต"):
+                addressTambon = data.split('.')[1]
+            if(data.split('.')[0] == "อ"):
+                addressAmphoe = data.split('.')[1]
+            if(data.split('.')[0] == "จ"):
+                addressProvince = data.split('.')[1]
+        mightBeZipcode = True
+        isAddressNo = False
     # 6	จำนวนแปลง
     # 7	ขนาดพื้นที่รวมทุกแปลง (ไร่)
     # 8	เป็นสมาชิกกลุ่ม
@@ -67,41 +86,204 @@ def createFarmer(records):
     lastName: '{lastName:}',
     dateOfBirth: Date.strptime('{dateOfBirth}', '%d-%m-%Y'),
     group: '{group:}',
-    addressNo: '{addressNo:}',
-    addressMoo: '{addressMoo:}',
-    addressTambon: '{addressTambon:}',
-    addressAmphoe: '{addressAmphoe:}',
-    addressProvince: '{addressProvince:}',
-    addressZipcode: '{addressZipcode:}',
-    phone: '{phone:}',
-    facebook: '{facebook:}',
-    line: '{line:}',
-    email: '{email:}'
+    addressNo: '{addressNo}',
+    addressMoo: '{addressMoo}',
+    addressTambon: '{addressTambon}',
+    addressAmphoe: '{addressAmphoe}',
+    addressProvince: '{addressProvince}',
+    addressZipcode: '{addressZipcode}',
+    phone: '{phone}',
+    facebook: '{facebook}',
+    line: '{line}',
+    email: '{email}'
     )"""
 
     return farmer
 
+def createPlot(records, farmer_id):
+    # 1	รหัสแปลง
+    # 2	รหัสเกษตรกร
+    # 3	ชื่อเกษตรกรเจ้าของแปลง
+    # 4	ขนาดพื้นที่ (ไร่)
+    areaRai = records[3][3]
+    # 5	จำนวนต้น (ต้น)
+    treeCount = records[4][3]
+    # 6	พันธุ์ที่ปลูก
+    breed = records[5][3].strip()
+    # 7	ปลูกรอบที่
+    # 8	ตำแหน่งที่ตั้ง (ที่อยู่แปลง)
+    addresses = records[7][3].strip().split(' ')
+    # ['32/9', 'หมู่', '3', 'ต.ศรีวิชัย', 'อ.พุนพิน', 'จ.สุราษฎร์ธานี']
+    # addressNo: "32/9",
+    # addressMoo: "3",
+    # addressTambon: "ศรีวิชัย",
+    # addressAmphoe: "พุนพิน",
+    # addressProvince: "สุราษฎร์ธานี",
+    # addressZipcode: "",
+    addressNo = ""
+    addressMoo = ""
+    addressTambon = ""
+    addressAmphoe = ""
+    addressProvince = ""
+    addressZipcode = ""
+
+    isAddressNo = True
+    nextIsMoo = False
+    mightBeZipcode = False
+    for data in addresses:
+        if(nextIsMoo):
+            nextIsMoo = False
+            addressMoo = data
+        elif(data.find('.') == -1):
+            if(data == "หมู่"):
+                nextIsMoo = True
+            elif(mightBeZipcode):
+                addressZipcode = data
+            elif(isAddressNo):
+                addressNo = data
+        else:
+            if(data.split('.')[0] == "ต"):
+                addressTambon = data.split('.')[1]
+            if(data.split('.')[0] == "อ"):
+                addressAmphoe = data.split('.')[1]
+            if(data.split('.')[0] == "จ"):
+                addressProvince = data.split('.')[1]
+        mightBeZipcode = True
+        isAddressNo = False
+
+    # 9	โครงการที่เข้าร่วม
+    project = records[8][3].strip()
+    # 10	ใบรับรองที่ได้ และวันที่ได้ใบรับรอง
+    certificate = ''
+    certificateDate = records[9][3].strip() if type(records[9][3]) == type("") else ""
+    # 11	ช่วงการเก็บเกี่ยวผลผลิต (วัน)
+    harvestPeriod = records[10][3].strftime('%B %Y') if type(records[9][3]) != float else ""
+    # 12	ปริมาณผลผลิตทั้งหมด (กิโลกรัม)
+    harvestQuantity = records[11][3]
+    # 13	ราคาผลผลิต (บาท/กิโลกรัม)
+    price = records[12][3]
+    # 14	การจัดการแปลง
+    plotManagement = {}
+    if records[13][3].find('√') != -1:
+        plotManagement["เกษตรอินทรีย์"] = "true"
+    if records[14][3].find('√') != -1:
+        plotManagement["อินทรีย์และเคมี"] = "true"
+    if records[15][3].find('√') != -1:
+        plotManagement["ตัดหญ้าดูแลแปลง"] = "true"
+    # 15	วิธีการใส่ปุ๋ย
+    count = 0
+    fertilizeManagement = {}
+    while records[16 + count][2] != "การจัดการน้ำ":
+        proc = {}
+        proc["สูตรปุ๋ย"] = records[16+count][3].strip().split(' ')[1] if records[16+count][3].strip().find(' ') != -1 else "-"
+        proc["อัตรา"] = records[16+count][4].strip().split(' ')[1] if len(records[16+count][4].strip().split(' ')) == 3 else "-"
+        fertilizeManagement[str(count)] = proc
+        count = count + 1
+    offset = count - 3
+    if(offset == 0):
+        proc = {}
+        proc["สูตรปุ๋ย"] = "-"
+        proc["อัตรา"] = "-"
+        fertilizeManagement[str(count)] = proc
+
+    # 16	การจัดการน้ำ
+    waterManagement = {}
+    if records[19+offset][3].find('√') != -1:
+        waterManagement["มีระบบน้ำหยด"] = "true"
+    if records[20+offset][3].find('√') != -1:
+        waterManagement["ไม่มีระบบน้ำ"] = "true"
+    # 17	การควบคุมโรค
+    illnessManagement = {}
+    if records[21+offset][3].find('√') != -1:
+        illnessManagement["พบโรคเถาเหี่ยว"] = records[21+offset][4].strip() + " " +records[21+offset][5].strip()
+    if records[22+offset][3].find('√') != -1:
+        illnessManagement["พบโรคราน้ำค้าง"] = records[22+offset][4].strip() + " " +records[22+offset][5].strip()
+    if records[23+offset][3].find('√') != -1:
+        illnessManagement["พบโรคเหี่ยวเขียว"] = records[23+offset][4].strip() + " " +records[23+offset][5].strip()
+    if records[24+offset][3].find('√') != -1:
+        illnessManagement["ไม่พบ"] = "true"
+    # 18	วิธีการเก็บเกี่ยวผลผลิต
+    harvestManagement = {}
+    if records[25+offset][3].find('√') != -1:
+        harvestManagement["ใช้แรงงานคน"] = "true"
+    if records[26+offset][3].find('√') != -1:
+        harvestManagement["ใช้เครื่องจักร"] = "true"
+    # 19	แหล่งรับซื้อ
+    sellingChannel = {}
+    if records[27+offset][3].find('√') != -1:
+        sellingChannel["เกษตรกรจำหน่ายเอง"] = "true"
+    if records[28+offset][3].find('√') != -1:
+        sellingChannel["เกษตรกรตัดพ่อค้าคนกลางมารับ"] = "true"
+    if records[29+offset][3].find('√') != -1:
+        sellingChannel["พ่อค้าคนกลางเหมายกแปลง"] = "true"
+    # 20	วิธีการขนส่งผลผลิต
+    logistic = {}
+    if records[30+offset][3].find('√') != -1:
+        logistic["มีรถมารับซื้อ"] = "true"
+    if records[31+offset][3].find('√') != -1:
+        logistic["รถบรรทุก"] = "true"
+    if records[32+offset][3].find('√') != -1:
+        logistic["รถกระบะ"] = "true"
+    # 21	ข้อมูลพิกัดตำแหน่งกลางแปลง (UTM)
+    lat = records[33+offset][3].strip().split(' ')[1] if records[33+offset][3].strip().find(' ') != -1 else 0
+    Long = records[33+offset][4].strip().split(' ')[1] if records[33+offset][4].strip().find(' ') != -1 else 0
+    # 22	ขอบเขตแปลง (รูปร่างของแปลงเกษตรกร)
+    # 23	รูปถ่ายบริเวณแปลง รูปผลผลิต
+
+    plot = f"""Plot.new(
+    farmer_id: {farmer_id},
+    areaRai: {areaRai},
+    treeCount: {treeCount},
+    breed: '{breed}',
+    project: '{project}',
+    certificate: '{certificate}',
+    certificateDate: '{certificateDate}',
+    harvestPeriod: '{harvestPeriod}',
+    harvestQuantity: '{harvestQuantity}',
+    price: '{price}',
+    plotManagement: '{str(plotManagement).replace("'",'"')}',
+    fertilizeManagement: '{str(fertilizeManagement).replace("'",'"')}',
+    waterManagement: '{str(waterManagement).replace("'",'"')}',
+    illnessManagement: '{str(illnessManagement).replace("'",'"')}',
+    harvestManagement: '{str(harvestManagement).replace("'",'"')}',
+    sellingChannel: '{str(sellingChannel).replace("'",'"')}',
+    logistic: '{str(logistic).replace("'",'"')}',
+    addressNo: '{addressNo}',
+    addressMoo: '{addressMoo}',
+    addressTambon: '{addressTambon}',
+    addressAmphoe: '{addressAmphoe}',
+    addressProvince: '{addressProvince}',
+    addressZipcode: '{addressZipcode}',
+    lat: '{lat}',
+    long: '{Long}'
+    )"""
+    # logistic: '{'มีรถมารับซื้อ':'true','รถกระบะ':'true'}',
+    return plot
+
 
 seedFile = open('seed.txt','w')
 filenames = sorted(glob.glob("./xls/*"))
-farmerCount = 1
+farmer_id = 1
 for filename in filenames:
-    print(f"{farmerCount}: read file - {filename}")
+    print(f"{farmer_id}: read file - {filename}")
     excel = pandas.ExcelFile(filename)
     for i in range(len(excel.sheet_names)):
         # Farmer Records
         if i == 0:
-            objName = f"farmer{farmerCount}"
-            print(f"{farmerCount}: - {objName}")
-            dfs = pandas.read_excel(filename,0)
+            objName = f"farmer{farmer_id}"
+            print(f"{farmer_id}: - {objName}")
+            dfs = pandas.read_excel(excel,0)
             seedFile.write( f"{objName} = {createFarmer(dfs.to_records())}\n" )
-            seedFile.write( attachPicture(objName) + "\n" )
+            seedFile.write( attachPicture(objName,'picture') + "\n" )
             seedFile.write( f"{objName}.save\n")
-            print(f"{farmerCount}: - {objName} [DONE]")
+            print(f"{farmer_id}: - {objName} [DONE]")
         # Plot Records
         elif i < len(excel.sheet_names) - 1:
-            objName = f"plot{farmerCount}_{i}"
-            print(f"{farmerCount}: - {objName}")
-            print('a')
-            print(f"{farmerCount}: - {objName} [DONE]")
-    farmerCount = farmerCount + 1
+            objName = f"plot{farmer_id}_{i}"
+            print(f"{farmer_id}: - {objName}")
+            dfs = pandas.read_excel(excel,i)
+            seedFile.write( f"{objName} = {createPlot(dfs.to_records(), farmer_id)}\n" )
+            seedFile.write( attachPicture(objName, 'pictures') + "\n" )
+            seedFile.write( f"{objName}.save\n")
+            print(f"{farmer_id}: - {objName} [DONE]")
+    farmer_id = farmer_id + 1
