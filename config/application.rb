871df32bbf4160166819273e8729f680ae6fd5b9
akraradets <akraradets@gmail.com>
2020-05-20 02:28:14 +0700

    add new config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,13 @@ module Tangmo
     config.load_defaults 5.2
     config.version = "1.0.2"
     config.date = "20-05-2020"
+    config.project = "โครงการวิจัยและพัฒนาการผลิตแตงโมบ้านทุ่งอ่าว"
+    # #3e2723 brown darken-4
+    # #ffcdd2 red lighten-4
+    # #424242 grey darken-3
+    config.acentColour = "#3e2723"
+    config.navbar = "navbar-dark"
+    config.footerTextColor = "white"
 
     config.title = "Tangmo"
     config.logo = "TANGMO_LOGO.png"
